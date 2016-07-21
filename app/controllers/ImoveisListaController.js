3f10527b9eef63a48d30ee66cce02798613c008d
Robert <robert.gol.jr@gmail.com>
2016-07-21 01:41:56 -0300

    busca valores

--- app/controllers/ImoveisListaController.js
+++ app/controllers/ImoveisListaController.js
@@ -7,6 +7,8 @@
 
   function ImoveisListaController($scope, Imoveis) {
     $scope.imoveis = [];
+    $scope.valorMax = 200000;
+    $scope.valorMin = 0;
     var promise = Imoveis.getImoveis();
     promise.then(function(data) {
       $scope.imoveis = data.data;
