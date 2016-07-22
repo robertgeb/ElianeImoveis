3045aea95bbba3aab99e247b66c92f09c3e744c7
Robert <robert.gol.jr@gmail.com>
2016-07-22 15:19:41 -0300

    mapa pagina contato

--- app/controllers/ContatoController.js
+++ app/controllers/ContatoController.js
@@ -6,5 +6,17 @@
   ContatoController.$inject = ['$scope'];
 
   function ContatoController($scope) {
+    var latlng = {lat: -22.969454, lng: -43.695457};
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+      center: latlng,
+      zoom: 15
+    });
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map,
+      title: 'Eliane Imóveis'
+    });
+
   };
 })();
